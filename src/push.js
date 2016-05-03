8468d165b67a199bf356b26b166de136ba706de1
Rudy <drawncloser@gmail.com>
2016-05-02 22:30:57 -0500

    Token listing

--- src/push.js
+++ src/push.js
@@ -45,12 +45,28 @@ module.exports = function(config) {
     }
   };
 
+  push._handleResp = function(parsed) {
+    var errors = [];
+    if (parsed.error) {
+      if (parsed.error.details) {  // Get the description of each error that happened
+        parsed.error.details.forEach(function(err) {
+          errors.push(err);
+        });
+      } else {  // It's a general error, just grab it
+        errors.push(parsed.error);
+      }
+    }
+
+    return errors;
+  };
+
   /**
    * Sender function, self explanatory
    *
    * @param {String} notification the notification object as described on http://docs.ionic.io/docs/push-sending-push
    */
   push.send = function(notification) {
+    var self = this;
     var deferred = q.defer();
     var errors = [];
 
@@ -75,15 +91,9 @@ module.exports = function(config) {
       res.setEncoding('utf8');
       res.on('data', function(resp) {
         var parsed = JSON.parse(resp);
-        if (parsed.error) {
-          if (parsed.error.details) {  // Get the description of each error that happened
-            parsed.error.details.forEach(function(err) {
-              errors.push(err);
-            });
-          } else {  // It's a general error, just grab it
-            errors.push(parsed.error);
-          }
-        }
+        self._handleResp(parsed).forEach(function(err) {
+          errors.push(err);
+        });
 
         // Update the promise
         if (errors.length) {
@@ -115,6 +125,7 @@ module.exports = function(config) {
    * @param {String} uuid the notification uuid
    */
   push.status = function(uuid) {
+    var self = this;
     var deferred = q.defer();
     var errors = [];
 
@@ -134,15 +145,9 @@ module.exports = function(config) {
       res.setEncoding('utf8');
       res.on('data', function(resp) {
         var parsed = JSON.parse(resp);
-        if (parsed.error) {
-          if (parsed.error.details) {  // Get the description of each error that happened
-            parsed.error.details.forEach(function(err) {
-              errors.push(err);
-            });
-          } else {  // It's a general error, just grab it
-            errors.push(parsed.error);
-          }
-        }
+        self._handleResp(parsed).forEach(function(err) {
+          errors.push(err);
+        });
 
         // Update the promise
         if (errors.length) {
@@ -165,7 +170,62 @@ module.exports = function(config) {
     req.end();
 
     return deferred.promise;
-  }
+  };
+
+  /**
+   * Get an array of recentl tokens
+   */
+  push.tokens = function() {
+    var self = this;
+    var deferred = q.defer();
+    var errors = [],
+        tokens = [];
+
+    // Create request opts
+    var options = {
+      hostname: 'api.ionic.io',
+      path: '/push/tokens',
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + this.jwt
+      }
+    };
+
+    // Create request
+    var req = internet.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function(resp) {
+        var parsed = JSON.parse(resp);
+        self._handleResp(parsed).forEach(function(err) {
+          errors.push(err);
+        });
+
+        // Update the promise
+        if (errors.length) {
+          deferred.reject(errors);
+        } else if (parsed.data) {
+          parsed.data.forEach(function(tok) {
+            tokens.push(tok.token);
+          })
+          deferred.resolve(tokens);
+        } else {
+          errors.push("ERROR: Server returned no data");
+          deferred.reject(errors);
+        }
+      });
+    });
+
+    req.on('error', function(error) {
+      errors.push(error);
+      deferred.reject(errors);
+    });
+
+    // Get that status!
+    req.end();
+
+    return deferred.promise;
+  };
 
   return new PushService(config);
 }
